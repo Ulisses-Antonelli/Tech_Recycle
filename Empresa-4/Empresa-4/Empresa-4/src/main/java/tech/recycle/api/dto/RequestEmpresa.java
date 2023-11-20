63740adbd1ab1a69e717e6981b862dbd8b3747ae
Reryson Andrade <112758537+AndradeReryson@users.noreply.github.com>
2023-11-20 12:52:55 -0300

    Delete Empresa-4 directory

--- Empresa-4/Empresa-4/Empresa-4/src/main/java/tech/recycle/api/dto/RequestEmpresa.java
+++
@@ -1,6 +0,0 @@
-package tech.recycle.api.dto;
-
-
-public record RequestEmpresa(Long id, String estabelecimento, String tipoEstabelecimento, String cnpj, String endereçoEstabelecimento, String email, String senha ) {
-    
-}
--- Empresa-4/Empresa-4/Empresa-4/src/main/java/tech/recycle/api/model/IEmpresaRepository.java
+++
@@ -1,7 +0,0 @@
-package tech.recycle.api.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IEmpresaRepository extends JpaRepository<Empresa, Long>{
-    
-}

c95be5f73ff334bc110683366142e0e4f622466d
AndradeReryson <reryson.andrade@fatec.sp.gov.br>
2023-11-23 00:00:35 -0300

    Progresso no desenv. da tela de lançar pontos + suas funcionalidades básicas

--- API_R01/src/main/java/tech/recycle/api/controller/PontosController.java
+++ API_R01/src/main/java/tech/recycle/api/controller/PontosController.java
@@ -1,5 +1,39 @@
 package tech.recycle.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tech.recycle.api.repository.EmpresaRepository;
+import tech.recycle.api.repository.PontosRepository;
+import tech.recycle.api.repository.UsuarioRepository;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("pontos")
 public class PontosController {
     
+    @Autowired
+    private PontosRepository repository;
+
+    @Autowired
+    private EmpresaRepository empresa_repo;
+
+    @Autowired
+    private UsuarioRepository usuario_repo;
+
+    @CrossOrigin
+    @GetMapping
+    public ResponseEntityity<Page<Pontos>> listaTodasTransacoes
+    (@PageableDefault(size = 10, sort = {"id"}) Pageable paginacao){
+        var page = repository.findAllPromocao(paginacao);
+
+        return ResponseEntity.status(200).body(page);
+    }
+    
+
+
 }
